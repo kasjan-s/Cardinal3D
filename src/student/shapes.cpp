56983e0093f4a511587be1443153a2a75c7aee26
Kasjan Siwek <kasjan.siwek@gmail.com>
2025-02-23 13:10:33 -0800

    Sphere intersection

--- src/student/shapes.cpp
+++ src/student/shapes.cpp
@@ -28,10 +28,50 @@ Trace Sphere::hit(const Ray& ray) const {
     Trace ret;
     ret.origin = ray.point;
     ret.hit = false;       // was there an intersection?
-    ret.distance = 0.0f;   // at what distance did the intersection occur?
-    ret.position = Vec3{}; // where was the intersection?
-    ret.normal = Vec3{};   // what was the surface normal at the intersection?
-    return ret;
+    // ret.distance = 0.0f;   // at what distance did the intersection occur?
+    // ret.position = Vec3{}; // where was the intersection?
+    // ret.normal = Vec3{};   // what was the surface normal at the intersection?
+
+    if (!bbox().hit(ray, ray.dist_bounds)){
+        return ret;
+    }
+
+    float a = ray.dir.norm_squared();
+    float b = 2 * dot(ray.point, ray.dir);
+    float c = ray.point.norm_squared() - radius;
+
+    float discriminant = b * b - 4 * a * c;
+    if (discriminant < 0) {
+        return ret;
+    } else if (discriminant == 0) {
+        float t = (-b) / (2*a);
+        if (t < ray.dist_bounds[0] || t > ray.dist_bounds[1]) {
+            return ret;
+        } else {
+            ret.hit = true;
+            ret.distance = t;
+            ret.position = ray.at(t);
+            ret.normal = ret.position;
+            return ret;
+        }
+    } else {
+        float t1 = (-b - sqrt(discriminant)) / (2 * a);
+        float t2 = (-b + sqrt(discriminant)) / (2 * a);
+
+        if (t1 >= ray.dist_bounds[0] && t1 <= ray.dist_bounds[1]) {
+            ret.hit = true;
+            ret.distance = t1;
+            ret.position = ray.at(t1);
+            ret.normal = ret.position;
+        } else if (t2 >= ray.dist_bounds[0] && t1 <= ray.dist_bounds[1]) {
+            ret.hit = true;
+            ret.distance = t2;
+            ret.position = ray.at(t2);
+            ret.normal = ret.position;
+        }
+
+        return ret;
+    }
 }
 
 } // namespace PT
